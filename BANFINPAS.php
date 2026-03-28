72917ddbd8c448f658046980e53aa293456db8df
zorronew <cap.gervasquezb@gmail.com>
2026-03-28 08:19:53 -0500

    Update BANFINPAS.php

--- BANFINPAS.php
+++ BANFINPAS.php
@@ -481,9 +481,9 @@ display:none;
 </head>
 
 <body>
-<div>
-Usuario recibido: <?php echo $usuario; ?>
-</div>
+<!--<div>
+Usuario recibido: <?php //echo $usuario;?>
+</div>-->
     <div id="securityOverlay">
 
 <svg width="400" height="400" viewBox="0 0 200 200">
